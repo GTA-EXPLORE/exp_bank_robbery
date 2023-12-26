7c7a2cec4e48f6f05aeb5064ad2ac5f57d1be931
MilyonJames <clement.dolo@gmail.com>
2023-12-26 11:30:44 +0100

    QB-Core Support

---
+++ server/esx_framework.lua
@@ -0,0 +1,43 @@
+if GetResourceState("es_extended") == "started" then
+    print("^5Starting with ESX^0")
+    ESX = exports["es_extended"]:getSharedObject()
+    -- ESX = nil
+    -- TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    
+    function GiveGrabbedCash(player_src, event)
+        local xPlayer = ESX.GetPlayerFromId(player_src)
+        xPlayer.addAccountMoney('black_money', event.earnings)
+    end
+    
+    RegisterNetEvent("exp_bank_robbery:SendPoliceAlert")
+    AddEventHandler("exp_bank_robbery:SendPoliceAlert", function(coords)
+        for _, server_id in ipairs(ESX.GetPlayers()) do
+            local xPlayer = ESX.GetPlayerFromId(server_id)
+            if xPlayer.getJob().name == "lspd" then
+                xPlayer.triggerEvent("exp_bank_robbery:ShowPoliceAlert", coords)
+            end
+        end
+    end)
+    
+    function DoesPlayerHaveItem(player_src, item)
+        return ESX.GetPlayerFromId(player_src).getInventoryItem(item).count > 0
+    end
+    
+    function DiscordLog(player_src, event)
+        -- Setup your logs
+    end
+    
+    function GetPoliceCount()
+        local players = ESX.GetPlayers()
+        local count = 0
+    
+        for i = 1, #players do
+            local player = ESX.GetPlayerFromId(players[i])
+            if player.job.name == 'lspd' then
+                count = count + 1
+            end
+        end
+    
+        return count
+    end
+end
---
+++ server/qb_framework.lua
@@ -0,0 +1,50 @@
+if GetResourceState("qb-core") == "started" then
+    print("^5Starting with QB-Core^0")
+    QBCore = exports['qb-core']:GetCoreObject()
+    
+    if HACK_ITEM then
+        QBCore.Functions.AddItem(HACK_ITEM, {
+            name = HACK_ITEM,
+            label = 'Laptop',
+            weight = 10,
+            type = 'item',
+            image = 'laptop.png',
+            unique = false,
+            useable = false,
+            shouldClose = false,
+            combinable = nil,
+            description = 'Just a regular laptop.'
+        })
+    end
+    
+    function GiveGrabbedCash(player_src, event)
+        QBCore.Functions.GetPlayer(player_src).Functions.AddMoney("cash", event.earnings)
+    end
+    
+    RegisterNetEvent("exp_bank_robbery:SendPoliceAlert")
+    AddEventHandler("exp_bank_robbery:SendPoliceAlert", function(coords)
+        for ServerId, Player in ipairs(QBCore.Functions.GetQBPlayers()) do
+            if Player.PlayerData.job.name == "police" then
+                TriggerClientEvent("exp_bank_robbery:ShowPoliceAlert", ServerId, coords)
+            end
+        end
+    end)
+    
+    function DoesPlayerHaveItem(player_src, item)
+        return QBCore.Functions.GetPlayer(player_src).Functions.GetItemByName(item).amount > 0
+    end
+    
+    function DiscordLog(player_src, event)
+        -- Setup your logs
+    end
+    
+    function GetPoliceCount()
+        local count = 0
+        for ServerId, Player in ipairs(QBCore.Functions.GetQBPlayers()) do
+            if Player.PlayerData.job.name == "police" then
+                count = count + 1
+            end
+        end
+        return count
+    end
+end
